6e2ab9e237c2a625cfb8a2a2c185daac9df5d3ec
Zayd Selmani <zayd448@gmail.com>
2023-07-11 21:52:55 +0200

    byebye pixi

--- libs/pong/game-page/src/lib/models/Scene.ts
+++ libs/pong/game-page/src/lib/models/Scene.ts
@@ -9,9 +9,11 @@ import { Keyboard } from './Keyboard';
 
 export class Scene extends Container {
   private ball: Graphics;
-  private clampyVelocity = 5;
-  private direction: 1 | -1 = 1;
+  // Determines the speed of the ball
+  private ballVelocity = 10;
+  // Determines the speed which the player can move his bar
   private barVelocity = 6;
+  private direction: 1 | -1 = 1;
   private p1Bar: Graphics;
   private p2Bar: Graphics;
 
@@ -25,34 +27,38 @@ export class Scene extends Container {
     this.addChild(this.p1Bar);
     this.addChild(this.p2Bar);
 
-    Ticker.shared.add(this._updateBall, this);
     Ticker.shared.add(this._updateBar1, this);
+    Ticker.shared.add(this._updateBall, this);
   }
 
   private _updateBall(deltaTime: number) {
     const ball = this.ball.getBounds();
     const p1Bar = this.p1Bar.getBounds();
     const p2Bar = this.p2Bar.getBounds();
+    // Is colliding with player 1's bar
     const collWithBar1 =
-      ball.x <= p1Bar.x + p1Bar.width &&
+      ball.x < p1Bar.x + p1Bar.width &&
       ball.y > p1Bar.y - ball.height &&
       ball.y < p1Bar.y + p1Bar.height;
 
+    // Is colliding with player 2's bar
     const collWithBar2 =
-      ball.x + ball.width >= p2Bar.x &&
+      ball.x + ball.width > p2Bar.x &&
       ball.y > p1Bar.y - ball.height &&
       ball.y < p1Bar.y + p1Bar.height;
 
-    if (
-      ball.x > this.screenWidth - ball.width ||
-      ball.x <= 0 ||
-      collWithBar1 ||
-      collWithBar2
-    ) {
-      this.direction *= -1;
+    const collRightWall = ball.x > this.screenWidth - ball.width;
+    const collLeftWall = ball.x <= 0;
+
+    // If ball collides with a bar change direction
+    this.direction *= collWithBar1 || collWithBar2 ? -1 : 1;
+
+    if (collLeftWall) {
+      console.log('Game done');
+      Ticker.shared.stop();
     }
 
-    this.ball.x += this.clampyVelocity * deltaTime * this.direction;
+    this.ball.x += this.ballVelocity * deltaTime * this.direction;
   }
 
   private _updateBar1(deltaTime: number) {
